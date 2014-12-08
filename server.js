9bc76db0a7ee4f2928fb73f17a17ea2cc52fc515
sundippatel <sundip.patel@outlook.com>
2014-12-08 14:30:38 -0800

    connect to mongo

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 var http = require('http');
+
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://"+process.env("DB_HOST"), function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  } else {
+    console.err(err);
+  }
+});
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
