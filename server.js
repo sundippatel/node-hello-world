18cd18c5fdd7fb2af9a25cf62a902bff53177796
sundippatel <sundip.patel@outlook.com>
2015-01-09 17:46:52 -0800

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ MongoClient.connect("mongodb://"+process.env.DB_HOST, function(err, db) {
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Runnable!\n');
+  res.end('Hello Runnable! Notifications are awesome!!\n');
 }).listen(80);
 console.log('Server running at http://127.0.0.1:80/');
