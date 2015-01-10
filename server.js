7994506e18332a3f9b5cab1f1e7f99217cb87822
sundippatel <sundip.patel@outlook.com>
2015-01-09 18:24:50 -0800

    removing spaces

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ var http = require('http');
   }
 });
 */
-
-
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Runnable! Notifications are awesome!!\n');
