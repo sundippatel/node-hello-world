15ba8700a20f5359be29d87a8d8ff4a206a2fba3
sundippatel <sundip.patel@outlook.com>
2015-01-12 18:42:23 -0800

    Doobie

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Runnable! Notifications are awesome!!\n');
+  res.end('Hello Runnable! Notifications are working?!\n');
 }).listen(80);
 console.log('Server running at http://127.0.0.1:80/');
