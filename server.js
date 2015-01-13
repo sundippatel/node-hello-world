40ac759170673b9da6081fb3b1746d8e67442b7c
sundippatel <sundip.patel@outlook.com>
2015-01-12 19:04:06 -0800

    disabled autopush

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Runnable! Notifications are working!\n');
+  res.end('Hello Runnable! Notifications are not working!\n');
 }).listen(80);
 console.log('Server running at http://127.0.0.1:80/');
