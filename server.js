2ced40350ef12abb6f647b8b16d51d070d8138f2
sundippatel <sundip.patel@outlook.com>
2015-09-09 17:10:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Runnable! Again, Testing notifications!\n');
-  console.log("Ensure Runnablecache is active.")
+  console.log("Moment of truth.")
 }).listen(80);
 console.log('Server running at http://127.0.0.1:80/');
